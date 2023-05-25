69519252110ce8d5761397d6b061bafea10f01ba
Daehan Ji <114852681+SpaceXdaehan@users.noreply.github.com>
2023-05-25 23:38:18 +0900

    Revert "20230525-1-mainupdate"
    This reverts commit 589dd0902483684dd0bcb0cf527c5d58b8464006.

--- alcohol/src/main/java/com/mid/alcohol/domain/Auction.java
+++ alcohol/src/main/java/com/mid/alcohol/domain/Auction.java
@@ -11,15 +11,14 @@ import lombok.Data;
 @AllArgsConstructor
 @Builder
 public class Auction {
-	// 경매 테이블
-    private int id; // 경매코드
-    private int productId; // 경매상품 테이블 참조하는 상품코드
-    private String aucioneer; // 경매 개최자
-    private String bidder; // 입찰자
-    private long passBid; // 즉시낙찰가
-    private long nowBid; // 현재입찰가
-    private long bidCount; // 입찰횟수
-    private long successBid; // 낙찰확정가
-    private LocalDateTime auctionStart; // 경매시작
-    private LocalDateTime autionEnd; // 경매 종료
+
+    private int number;
+    private int productId;
+    private String aucioneer;
+    private String bidder;
+    private long passId;
+    private long nowBid;
+    private long bidCount;
+    private long successBid;
+    private LocalDateTime auctionStart;
 }
--- alcohol/src/main/java/com/mid/alcohol/domain/AuctionProducts.java
+++ alcohol/src/main/java/com/mid/alcohol/domain/AuctionProducts.java
@@ -8,12 +8,11 @@ import lombok.Data;
 @AllArgsConstructor
 @Builder
 public class AuctionProducts {
-    // 경매물품 테이블
-	
-    private int productId; // 상품코드
-    private String pName; // 상품명
-    private String category; // 분류
-    private String constuctor; // 제조사(생산자)
-    private String brandName; // 브랜드명
-    private long cost; // 경매품 원가(매입가)
+    
+    private int productId;
+    private String pName;
+    private String category;
+    private String constuctor;
+    private String brandName;
+    private long cost;
 }
