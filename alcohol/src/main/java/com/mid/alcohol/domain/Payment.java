25597c5d5f00e7039591adb8f55ec942c151e82e
Daehan Ji <114852681+SpaceXdaehan@users.noreply.github.com>
2023-05-27 23:35:50 +0900

    Update Payment.java
    modify

--- alcohol/src/main/java/com/mid/alcohol/domain/Payment.java
+++ alcohol/src/main/java/com/mid/alcohol/domain/Payment.java
@@ -1,30 +1,25 @@
 package com.mid.alcohol.domain;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@Getter
-@Setter
-@ToString
 public class Payment {
-	private int memberId;
-	private String userId;
-    private String memberPassword;
-    private String memberName;
-    private String memberAge;
-    private String memberGender;
-    private String memberadress;
-    private String memberPhone;
-    private String memberEmail;
-    private Timestamp memberBirthday;
+	private int memberId; // TODO:
+	private String userId; // 회원 아이디
+    private String memberPassword;	// 회원 비밀번호
+    private String memberName; // 회원 이름
+    private String memberAge; // 회원 나이
+    private String memberGender; // 회원 성별
+    private String memberadress; // 회원 주소
+    private String memberPhone; // 회원 전화번호
+    private String memberEmail; // 회원 이메일
+    private Timestamp memberBirthday; // 회원 생년월일
 
 }
