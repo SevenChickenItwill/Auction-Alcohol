0f11e2d33955178664e34cefab549a95a77e77da
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2023-05-30 13:44:42 +0900

    5월 30일 업데이트

---
+++ alcohol/src/main/java/com/mid/alcohol/dto/CommentUpdateDto.java
@@ -0,0 +1,16 @@
+package com.mid.alcohol.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CommentUpdateDto {
+    private String content;
+    
+}
