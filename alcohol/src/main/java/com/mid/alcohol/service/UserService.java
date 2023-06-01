1d947f6b0ee49597c1def76d9f03a40d62c7c703
DoooHyun <brandena@naver.com>
2023-06-01 17:37:56 +0900

    ㅇㅇ

---
+++ alcohol/src/main/java/com/mid/alcohol/service/UserService.java
@@ -0,0 +1,30 @@
+package com.mid.alcohol.service;
+
+import org.springframework.stereotype.Repository;
+
+import com.mid.alcohol.domain.User;
+import com.mid.alcohol.dto.UserSignupDto;
+import com.mid.alcohol.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Repository
+public class UserService {
+
+    private final UserRepository userRepository;
+    
+    
+    public int signup(UserSignupDto dto) {
+        log.info("signup({})", dto);
+        
+          
+        
+        // 회원가입 처리
+        return userRepository.userMainSignup(dto.toEntity());
+
+    }
+    
+}
